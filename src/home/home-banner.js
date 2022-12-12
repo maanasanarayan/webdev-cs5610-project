1179d50ad49a3c670f7e2c3e62853c5f807032ae
maanasanarayan <mnsnryn@gmail.com>
2022-12-11 23:12:30 -0800

    Anonymous homepage fixes

--- src/home/home-banner.js
+++ src/home/home-banner.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const HomePageBanner = () => {
   return (
-    <div className="wd-home-banner ps-5 pt-4">
-      <div className="col-md-3 col-sm-12 text-center">
+    <div className="wd-home-banner p-5">
+      <div className="col-md-12 col-sm-12 text-center">
         <h2 className="text-white">Start investing today!</h2>
-        <button className="btn btn-light rounded-pill mt-3 w-50 wd-color-blue">
+        <button className="btn btn-light rounded-pill mt-3 ps-4 pe-4 wd-color-blue">
           Get Started
         </button>
       </div>
