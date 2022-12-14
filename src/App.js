55cf461cfcc3755aa0f8ad963c69e8a6c6f4e443
DESKTOP-88L6VBB\91903 <hkvin4@gmail.com>
2022-12-14 13:57:56 -0500

    main revert

--- src/App.js
+++ src/App.js
@@ -2,30 +2,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Routes, Route } from "react-router";
 import HomeComponent from "./home";
 import NavigationComponent from "./navigation";
-<<<<<<< HEAD
-import Login from "./pages/Login/Login";
-import SignUp from "./pages/Signup";
-import UserDetails from "./pages/dashboard";
-import EditProfile from "./pages/editProfile"
-function App() {
-  const user = localStorage.getItem("token");
 
-
-  return (
-    <>
-      <BrowserRouter>
-       <NavigationComponent/> 
-        <div className="container-fluid">
-          <Routes>
-            <Route path="/*" element={<HomeComponent />} />
-              <Route path="/sign-in" element={<Login />} />
-              <Route path="/sign-up" element={<SignUp />} />
-              {user && <Route path="/userDetails" element={<UserDetails />} />}
-              {user && <Route path="/editProfile" element={<EditProfile />} />}
-            
-          </Routes>
-        </div>
-=======
 import newsReducer from "./reducers/news-reducer";
 import stockReducer from "./reducers/stocks-reducer";
 import userReducer from "./reducers/user-reducer";
@@ -67,7 +44,7 @@ function App() {
           </div>
           <Footer />
         </Provider>
->>>>>>> 280094200b0c35afe02a70aafcff5d06625f1be2
+
       </BrowserRouter>
     </>
   );
